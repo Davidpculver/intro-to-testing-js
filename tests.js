db492acacb4123efb08812b4ce59d760d29d43fb
David Culver <culver.david.p@gmail.com>
2021-03-24 16:23:50 -0500

    add the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -1,4 +1,5 @@
 // Unit tests for the helloWorld function
+//describing helloworld function. spec defn is a function - houses all of the individual tests that exist in suite of test. each individual function is described by it. It function takes in string (description of individual test), function behind the individual test. "expect function" where building out the logic. (Expect: if entered into the console... to be: what we would expect returned)
 describe('helloWorld', function() {
     it('should be a defined function', function() {
       expect(typeof helloWorld).toBe('function');
@@ -13,3 +14,50 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+describe("addOne", function () {
+    it("should be a defined function", function () {
+        expect(typeof addOne).toBe("function");
+    });
+    it("should return a number when called", function () {
+        expect(typeof addOne()).toBe("number");
+    })
+    it("should return the number 3 when passed the argument of the number 2", function () {
+        expect(addOne(2)).toBe(3);
+    })
+    it("should return the number -43 when passed the argument of the number -44", function () {
+        expect(addOne(-44)).toBe(-43);
+    })
+    it("should return the number 18 when passed the argument of the number 17", function () {
+        expect(addOne(17)).toBe(18);
+    })
+    it("should return the number -2.14 when passed the argument of the number -3.14", function () {
+        expect(addOne(-3.14)).toBe(-2.14);
+    })
+    it("should return the number 4.58 when passed the argument of the number 3.58", function () {
+        expect(addOne(3.58)).toBe(4.58);
+    })
+    it("should return NaN when passed the string 'Michael Jordan'", function () {
+        expect(Number.isNaN(addOne("Michael Jordan"))).toBe(true);
+    })
+    it("should return NaN when passed the boolean true", function () {
+        expect(Number.isNaN(addOne(true))).toBe(true);
+    })
+    it("should return NaN when passed the array [1,2,3]", function () {
+        expect(Number.isNaN(addOne([1,2,3]))).toBe(true);
+    })
+});
+
+
+describe("sayHello", function () {
+    it("should be a defined function", function () {
+        expect(typeof sayHello).toBe("function")
+    })
+})
+
+
+
+
+
+
